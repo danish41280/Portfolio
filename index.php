1f4423a44ccab66c1f5c566e12f5d51453bd5a7a
danish41280 <danish41280@gmail.com>
2019-05-28 12:56:13 -0700

    Removed portfolio Directory

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Blog login</title>
+	<link href="assest/img/icon.png" rel="icon">
+	<meta name="PORTFOLIO" content="content">
+	<meta name="viewport" content="width=device-width, intial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" type="text/css" href="assest/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assest/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assest/css/material-icons.css">
+	<link rel="stylesheet" type="text/css" href="assest/fonts/css/all.css">
+	<link rel="stylesheet" type="text/css" href="assest/css/main.css">
+
+</head>
+<body data-spy="scroll" data-target="#navbarResponsive">
+
+<!----Start Home section------>
+<div id="home">
+					<!---Navigation--->
+	<nav class="navbar navbar-expand-md  fixed-top">
+		<a class="navbar-brand" href="#">
+			<img src="assest/img/logo.png">
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span><i class="fas fa-bars"></i></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a href="#home" class="nav-link">Home</a>
+				</li>
+				<li class="nav-item">
+					<a href="#about" class="nav-link">About</a>
+				</li>
+				<li class="nav-item">
+					<a href="#services" class="nav-link">Services</a>
+				</li>
+				<li class="nav-item">
+					<a href="#portfolio" class="nav-link">Portfolio</a>
+				</li>
+				<li class="nav-item">
+					<a href="#testimonial" class="nav-link">Testimonial</a>
+				</li>
+				<li class="nav-item">
+					<a href="#contact" class="nav-link">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<!--End nav -->
+
+	<!-- Start landing page  section -->
+	<div class="landing">
+		<div class="home-wrap">
+			<div class="home-inner">
+				<div class="fullscreen-video-wrap">
+					<video src="assest/video/code-2.mov" autoplay="true" loop="true"></video>
+				</div>
+				<div class="overlay"></div>
+			</div>
+		</div>
+	</div>
+	<div class="caption text-center">
+		<h4> Welcome to my website </h4>
+		<h1> Web Designer </h1>
+		<a href="contact" class="btn btn-outline-light btn-lg">Contact me</a>
+	</div>
+<!-- End Langding page section -->
+<!----End Home section------>
+
+<!----Start About section------>
+<div id="about" class="offset">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4">
+				<h1> Danish Azhar </h1>
+				<p> Front end developer </p>
+				<p> I am a WordPress & Front-end Developer, working for the last five years. I have experience working with local clients along with clients from all around the world. I have vast knowledge in html, css, Javascript , JQuery, Angular (javascript framewok), wordpress theme development and so on. </p>
+			</div>
+			<div class="col-lg-4">
+				<img src="assest/img/mypic.jpeg" style="width: auto; height: 400px;">
+			</div>
+			<div class="col-lg-4">
+				<div class="wrapper">
+					<h2> HTML/CSS </h2><span class="bar"><span class="html"></span></span>
+					<div class="line line1">90%</div>
+					<h2> JAVASCRIPT </h2><span class="bar"><span class="javascript"></span></span>
+					<div class="line line1">50%</div>
+					<h2> PHP </h2><span class="bar"><span class="php"></span></span>
+					<div class="line line1">60%</div>
+					<h2> LARAVEL </h2><span class="bar"><span class="laravel"></span></span>
+					<div class="line line1">30%</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!----End About section------>
+
+<!----Start Services section------>
+<div id="services" class="offset">
+	<div class="container">
+		<h2> What I Offer </h2>
+		<div class="row">
+			<div class="col-lg-3 col-md-3">
+				<i class="fas fa-desktop" aria-hidden="true"></i>
+				<h4> Web design </h4>
+				<p> We provide the best website layout </p>
+			</div>
+			<div class="col-lg-3 col-md-3">
+				<i class="fas fa-mobile" aria-hidden="true"></i>
+				<h4> Mobile Apps </h4>
+				<p> We provide the best website layout </p>
+			</div>
+			<div class="col-lg-3 col-md-3">
+				<i class="fas fa-mobile" aria-hidden="true"></i>
+				<h4> Graphic design </h4>
+				<p> We provide the best website layout </p>
+			</div>
+			<div class="col-lg-3 col-md-3">
+				<i class="fas fa-database" aria-hidden="true"></i>
+				<h4> Database </h4>
+				<p> We provide the best website layout </p>
+			</div>
+		</div>
+	</div>
+		<div class="jumbotorn background-image">
+			<img src="assest/img/shortimage.jpeg">
+			<h1> You Can Download My CV </h1> <br>
+			<a href="#" class="btn btn-outline-light Cv">Download Now</a>
+		</div>
+	
+</div>
+<!----End Services section------>
+
+<!----Start Portfolio section------>
+<div id="portfolio" class="offset">
+	<div class="container">
+		<h2 class="heading text-center"> My Work </h2>
+		<div class="row">
+			<div class="col-lg-4 imgBox">
+				<a href="#"><img src="assest/img/mypic.jpeg" data-text="This is danish"></a>
+			</div>
+			<div class="col-lg-4 imgBox">
+				<a href="#"><img src="assest/img/mypic.jpeg"></a>
+			</div>
+			<div class="col-lg-4 imgBox">
+				<a href="#"><img src="assest/img/mypic.jpeg"></a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4 imgBox">
+				<a href="#"><img src="assest/img/mypic.jpeg">
+				</a>
+			</div>
+			<div class="col-lg-4 imgBox">
+				<a href="#"><img src="assest/img/mypic.jpeg"></a>
+			</div>
+			<div class="col-lg-4 imgBox">
+				<a href="#"><img src="assest/img/mypic.jpeg"></a>
+			</div>
+		</div>
+		
+</div>
+	</div>
+
+<!----End Portfolio section------>
+
+<!----Start Testimonial section------>
+<div id="testimonial" class="offset">
+</div>
+<!----End Testimonial section------>
+
+<!----Start Contact section------>
+<div id="contact" class="offset">
+</div>
+<!----End Contact section------>
+
+
+
+
+
+
+
+
+	<script type="text/javascript" src="assest/js/jquery.min.js">	</script>
+	<script type="text/javascript" src="assest/js/bootstrap.js">	</script>
+	<script type="text/javascript" src="assest/js/bootstrap.min.js">	</script>
+	<script type="text/javascript" src="assest/js/main.js">	</script>
+	
+	
+
+</body>
+</html>
